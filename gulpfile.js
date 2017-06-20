045409828a07d0d5c31c6d4cfb5f095e6c70ce8c
Johnathan Rhoades <jrhoades7989@gmail.com>
2017-06-20 14:19:36 -0400

    Added sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
     browserify = require('gulp-browserify');
 
 var jsSources = [
     'process/scripts/scripts.js'
     ];
+
+var sassSources = ['process/sass/styles.scss'];
 
 gulp.task('js', function() {
     gulp.src(jsSources)
@@ -15,4 +18,13 @@ gulp.task('js', function() {
         .pipe(gulp.dest('build/development/js'))
         });
 
-
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'process/sass',
+            style: 'expanded'
+            }
+        ))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('build/development/css'))
+        });
