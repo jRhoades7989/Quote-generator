b6109a8bbd14c1a227c5fdfe4be8969d709f581c
Johnathan Rhoades <jrhoades7989@gmail.com>
2017-06-21 08:41:38 -0400

    Added Compression to Production Version

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,37 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     browserify = require('gulp-browserify');
 
-var jsSources = [
+var jsSources, 
+    sassSources, 
+    htmlSources, 
+    env, 
+    sassStyle,
+    outputDir;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    outputDir = 'build/development/';
+    sassStyle = 'expanded';
+} else {
+    outputDir = 'build/production/';
+    sassStyle = 'compressed';
+};
+
+jsSources = [
     'process/scripts/scripts.js'
 ];
 
-var sassSources = ['process/sass/styles.scss'];
+sassSources = ['process/sass/styles.scss'];
+
+htmlSources = [outputDir + '*.html'];
+
 
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('scripts.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('build/development/js'))
+        .pipe(gulp.dest(outputDir + '/js'))
         .pipe(connect.reload())
 });
 
@@ -24,26 +44,32 @@ gulp.task('compass', function() {
     gulp.src(sassSources)
         .pipe(compass({
             sass: 'process/sass',
-            cache_location: 'build/development/css',
-            style: 'expanded'
+            cache_location: outputDir + 'css',
+            style: sassStyle
         }
         ))
         .on('error', gutil.log)
-        .pipe(gulp.dest('build/development/css'))
+        .pipe(gulp.dest(outputDir + 'css'))
         .pipe(connect.reload())
 });
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+        });
 
 gulp.task('watch', function() {
     gulp.watch(jsSources, ['js']);
     gulp.watch('process/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
 });
 
 gulp.task('connect', function() {
     connect.server({
-        root: 'build/development/',
+        root: outputDir,
         livereload: true
         });
     });
 
-gulp.task('default', ['js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['js', 'compass', 'html', 'connect', 'watch']);
 
