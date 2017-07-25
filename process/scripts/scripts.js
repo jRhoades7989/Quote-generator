79c1723839595d4ead845948a435fffab224d0ec
Johnathan Rhoades <jrhoades7989@gmail.com>
2017-07-24 22:39:54 -0400

    Added Color Changing

--- process/scripts/scripts.js
+++ process/scripts/scripts.js
@@ -1,1 +1,25 @@
-var Jquery = require('jquery');
+/*jshint esversion: 6*/
+
+function makeColor() {
+    let hue = Math.floor(Math.random() * 366);
+    let sat = 50 + Math.floor(Math.random() * 31);
+    let lgt = 55 + Math.floor(Math.random() * 21); 
+    
+    return {"hue": hue,
+            "sat": sat,
+            "lgt": lgt};
+}
+
+window.onload= function() {
+    var quoteButton = document.querySelector("#quote-generator");    
+    var twitterButton = document.querySelector("#twitter");
+    var root = document.querySelector(":root");
+    
+    quoteButton.addEventListener("click", function() {
+        color = makeColor();
+        root.style.setProperty("--background-color", `hsl(${color.hue}, ${color.sat}%, ${color.lgt}%)`); 
+        root.style.setProperty("--background-color-13", `hsl(${color.hue}, ${color.sat}%, ${color.lgt - 13}%)`); 
+        root.style.setProperty("--background-color-10", `hsl(${color.hue}, ${color.sat}%, ${color.lgt - 10}%)`); 
+        
+    });
+};
